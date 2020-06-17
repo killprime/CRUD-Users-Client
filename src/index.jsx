773c5003f726f20542c18210530fd0cad6146661
killprime <killprime@mail.ru>
2020-06-18 08:12:54 +1000

    fix bags

--- src/index.jsx
+++ src/index.jsx
@@ -11,13 +11,12 @@ import { rootReducer } from './redux/rootReducer';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import 'bootstrap'; 
+import 'bootstrap';
 
 const store = createStore(rootReducer, compose(
   applyMiddleware(
     thunk
-  ),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 ));
 
 const history = createBrowserHistory()
