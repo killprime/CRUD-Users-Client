d03422804e822e0556e3611d760393a94415ff88
killprime <killprime@mail.ru>
2020-06-18 07:22:47 +1000

    init

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 //здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 //простой тест сервера
 app.get('/ping', function (req, res) {
@@ -16,6 +16,6 @@ return res.send('pong');
 
 //обслуживание html
 app.get('/*', function (req, res) {
-res.sendFile(path.join(__dirname, 'build', 'index.html'));
+res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(port);
